6a981d13d3da57008ebd742cc4a7e05ddc545fd3
yoonminsang <apdlyooapdl@naver.com>
2021-04-30 22:50:57 +0900

    complete

--- toDo.js
+++ toDo.js
@@ -1,33 +1,74 @@
 (function () {
   const TODOLIST = 'list';
 
-  const contentMenu = document.querySelector('.contnet_menu');
+  const form = document.querySelector('form');
   const toDoInput = document.querySelector('.toDo_input');
-  const toDoAdd = document.querySelector('.toDo_add');
   const toDoMultiDelete = document.querySelector('.toDo_multi_delete');
   const toDoList = document.querySelector('.toDo_list');
 
   let list;
   const loadToDoList = () => {
-    list = JSON.parse(localStorage.getItem(TODOLIST));
+    list = JSON.parse(localStorage.getItem(TODOLIST)) || [];
     list.forEach((v, i) => {
-      v['id'] = i + 1;
-      v['check'] = false;
+      v.id = i + 1;
+      v.check = false;
       getToDoList(v);
     });
   };
 
-  const toggleToDoList = (id) => {
-    for (let i = 0; i < list.length; i++) {
-      if (list[i]['id'] === id) {
-        list[i]['check'] = !list[i]['check'];
-        break;
-      }
-    }
+  const saveToDoList = () => {
+    const save = list.map((v) => {
+      const obj = Object.assign({}, v);
+      delete obj.id;
+      delete obj.check;
+      return obj;
+    });
+    localStorage.setItem(TODOLIST, JSON.stringify(save));
   };
+
+  const toggleToDoList = (id) => {
+    const findIdx = list.findIndex((v) => v.id === id);
+    list[findIdx].check = !list[findIdx].check;
+  };
+
+  const deleteMultiToDoList = () => {
+    const filter = list.filter((v) => v.check === true);
+    filter.forEach((v) => {
+      deleteToDoList(v.id);
+    });
+  };
+
   const deleteToDoList = (id) => {
     document.getElementById(id).remove();
+    const findIdx = list.findIndex((v) => v.id === id);
+    list.splice(findIdx, 1);
+    saveToDoList();
   };
+
+  const getTime = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) month = '0' + String(month);
+    const datee = date.getDate();
+    const day = date.toString().slice(0, 3);
+    const dateArr = [year, month, datee, day];
+    const time = date.toString().slice(16, 24);
+    return dateArr.join(' ') + '  ' + time;
+  };
+
+  const addToDoList = (e) => {
+    e.preventDefault();
+    const id = list[list.length - 1] ? list[list.length - 1].id + 1 : 1;
+    const toDo = toDoInput.value;
+    const date = getTime();
+    const check = false;
+    list.push({ id, toDo, date, check });
+    getToDoList(list[list.length - 1]);
+    saveToDoList();
+    toDoInput.value = '';
+  };
+
   const getToDoList = (v) => {
     const { id, toDo, date } = v;
     const div = document.createElement('div');
@@ -55,6 +96,8 @@
   };
   const init = () => {
     loadToDoList();
+    form.addEventListener('submit', addToDoList);
+    toDoMultiDelete.addEventListener('click', deleteMultiToDoList);
   };
   init();
 })();
