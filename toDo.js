30f2d2bf1bbbaf52dca8b45e52a64a3921d3d187
yoonminsang <apdlyooapdl@naver.com>
2021-04-30 16:50:02 +0900

    get

--- toDo.js
+++ toDo.js
@@ -1,16 +1,45 @@
 (function () {
   const TODOLIST = 'list';
 
-  const loadToDo = () => {
+  const contentMenu = document.querySelector('.contnet_menu');
+  const toDoInput = document.querySelector('.toDo_input');
+  const toDoAdd = document.querySelector('.toDo_add');
+  const toDoMultiDelete = document.querySelector('.toDo_multi_delete');
+  const toDoList = document.querySelector('.toDo_list');
+
+  const loadToDoList = () => {
     let load = JSON.parse(localStorage.getItem(TODOLIST));
     load.forEach((v, i) => {
       v['id'] = i + 1;
       v['checkList'] = false;
+      getToDoList(v);
     });
-    console.log(load);
+  };
+  const getToDoList = (v) => {
+    const { id, toDo, date } = v;
+    const div = document.createElement('div');
+    div.id = id;
+    div.className = 'toDo_list_item';
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    div.append(checkBox);
+    const textDiv = document.createElement('div');
+    textDiv.className = 'text';
+    textDiv.textContent = toDo;
+    div.append(textDiv);
+    const dateDiv = document.createElement('div');
+    dateDiv.className = 'date';
+    dateDiv.textContent = date;
+    div.append(dateDiv);
+    const deleteBtn = document.createElement('input');
+    deleteBtn.className = 'delete';
+    deleteBtn.type = 'button';
+    deleteBtn.value = '❌';
+    div.append(deleteBtn);
+    toDoList.append(div);
   };
   const init = () => {
-    loadToDo();
+    loadToDoList();
   };
   init();
 })();
