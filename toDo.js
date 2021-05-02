1538dbffb4e4a48d5ff48b1928437bcca90b9f4d
yoonminsang <apdlyooapdl@naver.com>
2021-05-02 14:56:43 +0900

    css

--- toDo.js
+++ toDo.js
@@ -50,7 +50,8 @@
     const year = date.getFullYear();
     let month = date.getMonth() + 1;
     if (month < 10) month = '0' + String(month);
-    const datee = date.getDate();
+    let datee = date.getDate();
+    if (datee < 10) datee = '0' + String(datee);
     const day = date.toString().slice(0, 3);
     const dateArr = [year, month, datee, day];
     const time = date.toString().slice(16, 24);
